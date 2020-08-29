38140be7e88ac26b5675fe194566d33e74cb970e
Copete Largacha Valeria <Valeria.Copete@carvajal.com>
2020-08-29 16:01:17 -0500

    Se cancelan las tareas realizadas

--- src/main.js
+++ src/main.js
@@ -2,15 +2,25 @@ let addNoteBtn = document.querySelector('#addNoteBtn');
 addNoteBtn.onclick = () => {
 
     //input forms
-    let newNoteInput =document.querySelector('#newNoteInput');
-    let notes = document.querySelector('#app .notes');   
-   //new elements
+    let newNoteInput = document.querySelector('#newNoteInput');
+    let notes = document.querySelector('#app .notes');
+    //new elements
     let note = document.createElement('li');
     let checkbox = document.createElement('input');
 
     checkbox.type = "checkbox";
+    //Para tachar la tarea realizada
+    checkbox.onclick = function () {
+        let parent = this.parentElement;
+        if (this.checked) {
+            parent.classList.add('isDone');
+        } else {
+            parent.classList.remove('isDone');
+        }
+    }
+
     note.classList.add('note');
-    note.innerHTML =newNoteInput.value;
+    note.innerHTML = `<div>${newNoteInput.value}</div>`;
     note.appendChild(checkbox);
     newNoteInput.value = '';
     notes.appendChild(note);
